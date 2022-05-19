60079dd492ba9e3b352dbc68919172f586b6a358
André <AndreR@ppedv.de>
2022-05-19 12:38:19 +0200

    Beste Debugging test anwendung

---
+++ HalloDebugger/HalloDebugger/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+#if RELEASE
+    Console.WriteLine("RELEASE VERSION");
+#else
+Console.WriteLine("DEBUG VERSION");
+#endif
+
+Console.WriteLine("Eine Taste drücken, dann geht es los");
+Console.ReadKey();
+
+var morgen = DateTime.Now.AddDays(1);
+var text = "Dies ist ein Text";
+var zahl = 12;
+
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Zahl {i}");
+    if (i == 12)
+    {
+        Debugger.Break();
+    }
+}
+
+SagHallo();
+
+Console.WriteLine("Ende");
+Console.ReadKey();
+
+void SagHallo()
+{
+    ZeigeTextInConsole("Hallo");
+    ZeigeTextInConsole("");
+    ZeigeTextInConsole(null);
+}
+
+void ZeigeTextInConsole(string text)
+{
+    Debug.Assert(text != null);
+
+    Console.WriteLine($"{text} [{text.Length}]");
+}

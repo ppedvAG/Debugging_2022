bf986d4503506562b87d5d7036d2d617d40a4dcd
André <AndreR@ppedv.de>
2022-05-20 10:47:50 +0200

    TPL VB

---
+++ TPL_VB/TPL_VB/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Start")
+
+        Dim po = New ParallelOptions With {
+            .MaxDegreeOfParallelism = 6
+        }
+
+        Parallel.For(0, 100000, po, Sub(i As Integer) Console.WriteLine(i))
+
+
+        'Parallel.Invoke(AddressOf Zähle, AddressOf Zähle, AddressOf Zähle, AddressOf Zähle)
+
+        Console.WriteLine("Ende")
+        Console.ReadKey()
+    End Sub
+
+    Private Sub Zähle()
+        For index = 1 To 10
+            Console.WriteLine(index)
+        Next
+    End Sub
+
+End Module

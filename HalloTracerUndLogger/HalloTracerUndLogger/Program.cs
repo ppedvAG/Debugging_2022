d6f65c2b2535e7438e52b4d1c96bd890921fb4f9
André <AndreR@ppedv.de>
2022-05-20 11:28:08 +0200

    Tracelogger

---
+++ HalloTracerUndLogger/HalloTracerUndLogger/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+Trace.Listeners.Add(new TextWriterTraceListener("trace.txt"));
+Trace.Listeners.Add(new EventLogTraceListener("Application"));
+Trace.AutoFlush = true;
+
+
+Debug.WriteLine("Hallo Debugger");
+Trace.WriteLine("Hallo Trace");
+Trace.TraceInformation("Hallo Trace Info");
+Trace.TraceWarning("Hallo Trace Warning");
+Trace.TraceError("Hallo Trace Error");
